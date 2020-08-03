7fd347fac026512c2beb324b2d7e6151b3b15c76
Spencer Carli <spencercarli@gmail.com>
2020-08-03 16:38:37 -0500

    First exercise

--- exercises/01.final.js
+++ exercises/01.final.js
@@ -4,47 +4,13 @@ import { View, Text } from "react-native";
 export default () => (
   <View
     style={{
-      flex: 1,
-      justifyContent: "space-around",
-      // justifyContent: "space-between",
-      // justifyContent: "center",
+      width: 100,
+      height: 100,
+      justifyContent: "center",
       alignItems: "center",
-      flexDirection: "row",
-      // flexDirection: 'column'
+      backgroundColor: "#7ce0f9",
     }}
   >
-    <View
-      style={{
-        width: 100,
-        height: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#7ce0f9",
-      }}
-    >
-      <Text>Square 1</Text>
-    </View>
-    <View
-      style={{
-        width: 100,
-        height: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#caeccf",
-      }}
-    >
-      <Text>Square 2</Text>
-    </View>
-    <View
-      style={{
-        width: 100,
-        height: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#76c4ae",
-      }}
-    >
-      <Text>Square 3</Text>
-    </View>
+    <Text>Square 1</Text>
   </View>
 );
--- exercises/01.js
+++ exercises/01.js
@@ -1,1 +1,17 @@
-export default () => null;
+// When using JSX React needs to be in scope. This means you have to import it.
+// import React from "react";
+
+// Unlike working with HTML, the building blocks of our UI are not global. Therefore we need to
+// import them.
+// import { View, Text } from "react-native";
+
+// TIP: A component is simply a function from which we return JSX.
+// const App = () => {}
+
+// TIP: Components can nest and have children. <View><View></View></View>
+
+// TIP: Styling happens via the style prop. That style, in it's most basic form, is simply an
+// object. style={{ ... }}
+
+// We need to export App/Component so it's rendered on the screen.
+// export default App;
