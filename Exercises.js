a05137b99d0cbf106790a5dab6ba55717ca2bb32
Spencer Carli <spencercarli@gmail.com>
2020-08-07 11:03:04 -0500

    Add tabs so you can switch between demos

--- Exercises.js
+++ Exercises.js
@@ -2,80 +2,170 @@ export default [
   {
     lesson: "hello-world",
     title: "Hello, world!",
-    source: require("./exercises/hello-world.js").default,
-    // source: require("./solutions/hello-world.final").default,
+    code: [
+      {
+        name: "Exercise",
+        source: require("./exercises/hello-world").default,
+      },
+      {
+        name: "Final",
+        source: require("./solutions/hello-world.final").default,
+      },
+    ],
     hintSource: require("./exercises/hello-world.md"),
   },
   {
     lesson: "capturing-taps",
     title: "Capturing Taps",
-    source: require("./exercises/capturing-taps.js").default,
-    // source: require("./solutions/capturing-taps.final").default,
-    // source: require("./solutions/capturing-taps.extra-1").default,
+    code: [
+      {
+        name: "Exercise",
+        source: require("./exercises/capturing-taps.js").default,
+      },
+      {
+        name: "Final",
+        source: require("./solutions/capturing-taps.final").default,
+      },
+      {
+        name: "Extra 1",
+        source: require("./solutions/capturing-taps.extra-1").default,
+      },
+    ],
     hintSource: require("./exercises/capturing-taps.md"),
   },
   {
     lesson: "custom-component",
     title: "Custom Component",
-    source: require("./exercises/custom-component").default,
-    // source: require("./solutions/custom-component.final").default,
-    // source: require("./solutions/custom-component.extra-1").default,
+    code: [
+      {
+        name: "Exercise",
+        source: require("./exercises/custom-component").default,
+      },
+      {
+        name: "Final",
+        source: require("./solutions/custom-component.final").default,
+      },
+      {
+        name: "Extra 1",
+        source: require("./solutions/custom-component.extra-1").default,
+      },
+    ],
     hintSource: require("./exercises/custom-component.md"),
   },
   {
     lesson: "state-props",
     title: "State & Props",
-    source: require("./exercises/state-props").default,
-    // source: require("./solutions/state-props.final").default,
-    // source: require("./solutions/state-props.extra-1").default,
+    code: [
+      { name: "Exercise", source: require("./exercises/state-props").default },
+      {
+        name: "Final",
+        source: require("./solutions/state-props.final").default,
+      },
+      {
+        name: "Extra 1",
+        source: require("./solutions/state-props.extra-1").default,
+      },
+    ],
     hintSource: require("./exercises/state-props.md"),
   },
   {
     lesson: "styling",
     title: "Styling",
-    source: require("./exercises/styling").default,
-    // source: require("./solutions/styling.final").default,
-    // source: require("./solutions/styling.extra-1").default,
+    code: [
+      { name: "Exercise", source: require("./exercises/styling").default },
+      { name: "Final", source: require("./solutions/styling.final").default },
+      {
+        name: "Extra 1",
+        source: require("./solutions/styling.extra-1").default,
+      },
+    ],
     hintSource: require("./exercises/styling.md"),
   },
   {
     lesson: "scrollable-content",
     title: "Scrollable Content",
-    source: require("./solutions/scrollable-content.final").default,
+    code: [
+      {
+        name: "Final",
+        source: require("./solutions/scrollable-content.final").default,
+      },
+    ],
     // hintSource: require("./exercises/scrollable-content.md"),
   },
   {
     lesson: "building-a-form",
     title: "Building a Form",
-    source: require("./solutions/building-a-form.final").default,
+    code: [
+      {
+        name: "Final",
+        source: require("./solutions/building-a-form.final").default,
+      },
+    ],
   },
   {
     lesson: "long-lists",
     title: "Long Lists",
-    // source: require("./solutions/long-lists.final").default,
-    source: require("./solutions/long-lists.extra-1").default,
+    code: [
+      {
+        name: "Final",
+        source: require("./solutions/long-lists.final").default,
+      },
+      {
+        name: "Extra 1",
+        source: require("./solutions/long-lists.extra-1").default,
+      },
+    ],
   },
   {
     lesson: "working-with-api",
     title: "Working with an API",
-    // source: require("./solutions/working-with-api.final").default,
-    // source: require("./solutions/working-with-api.extra-1").default,
-    source: require("./solutions/working-with-api.extra-2").default,
+    code: [
+      {
+        name: "Final",
+        source: require("./solutions/working-with-api.final").default,
+      },
+      {
+        name: "Extra 1",
+        source: require("./solutions/working-with-api.extra-1").default,
+      },
+      {
+        name: "Extra 2",
+        source: require("./solutions/working-with-api.extra-2").default,
+      },
+    ],
   },
   {
     lesson: "multiple-files",
     title: "Mulitple Files",
-    source: require("./solutions/multiple-files.final").default,
+    code: [
+      {
+        name: "Final",
+        source: require("./solutions/multiple-files.final").default,
+      },
+    ],
   },
   {
     lesson: "class-components",
     title: "Class Components",
-    source: require("./solutions/class-components.final").default,
+    code: [
+      {
+        name: "Final",
+        source: require("./solutions/class-components.final").default,
+      },
+    ],
   },
   {
     lesson: "component-lifecycle",
     title: "Component Lifecycle",
-    // source: require("./solutions/component-lifecycle.final").default,
-    source: require("./solutions/component-lifecycle.extra-1").default,
+    code: [
+      {
+        name: "Final",
+        source: require("./solutions/component-lifecycle.final").default,
+      },
+      {
+        name: "Extra 1",
+        source: require("./solutions/component-lifecycle.extra-1").default,
+      },
+    ],
   },
 ];
--- src/screens/Exercise.js
+++ src/screens/Exercise.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import _ from "lodash";
+import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
 
 import exercises from "../../Exercises";
 
 export const Exercise = ({ navigation, route }) => {
   const lesson = _.get(route, "params.lesson");
   const lessonData = _.find(exercises, (e) => e.lesson === lesson);
+  const [tabIndex, setTabIndex] = useState(0);
 
   useEffect(() => {
     if (!lessonData.hintSource) {
@@ -15,10 +17,36 @@ export const Exercise = ({ navigation, route }) => {
     }
   }, []);
 
-  if (lessonData.source) {
-    const Output = lessonData.source;
-
-    return <Output />;
+  if (lessonData.code && lessonData.code.length > 0) {
+    const Output = lessonData.code[tabIndex].source;
+    return (
+      <>
+        <View style={{ flex: 1 }}>{Output ? <Output /> : null}</View>
+        <SafeAreaView style={{ backgroundColor: "#fff" }}>
+          <View
+            style={{
+              paddingVertical: 20,
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            {lessonData.code.map((ex, index) => {
+              const selected = tabIndex === index;
+              return (
+                <TouchableOpacity
+                  key={ex.name}
+                  onPress={() => setTabIndex(index)}
+                >
+                  <Text style={selected ? { color: "red" } : {}}>
+                    {ex.name}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </SafeAreaView>
+      </>
+    );
   }
 
   return null;
