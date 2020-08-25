6cd101be645980759a7ec62384b905d8ba768d1a
Spencer Carli <spencercarli@gmail.com>
2020-08-25 11:04:49 -0500

    Welcome screen redesign

---
+++ src/constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+  brand: "#7CA1B4",
+  accent: "#7cb48f",
+};
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -3,47 +3,92 @@ import {
   View,
   Text,
   StyleSheet,
-  Button,
   SafeAreaView,
   Linking,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  StatusBar,
 } from "react-native";
+
+import colors from "../constants/colors";
+
+const screen = Dimensions.get("screen");
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "space-between",
+    backgroundColor: colors.brand,
   },
   title: {
     fontWeight: "bold",
-    fontSize: 20,
+    fontSize: 28,
     textAlign: "center",
     marginBottom: 20,
     paddingHorizontal: 20,
+    color: "#fff",
+    fontFamily: "Menlo",
   },
-  link: {
-    color: "#007aff",
+  btn: {
+    backgroundColor: colors.accent,
+    alignSelf: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 10,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  logo: {
+    width: screen.width * 0.8,
+    height: screen.width * 0.8,
+    resizeMode: "contain",
+    position: "absolute",
+    bottom: -80,
+    opacity: 0.2,
+  },
+  text: {
+    color: "#fff",
+    fontWeight: "500",
+    fontSize: 14,
   },
 });
 
 export const Welcome = ({ onOnboardingComplete = () => {} }) => {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.brand} />
       <View />
       <View>
         <Text style={styles.title}>
           Welcome to the React Native Fundamentals Workshop!
         </Text>
-        <Button title="Get Started" onPress={() => onOnboardingComplete()} />
-      </View>
-      <Text>
-        {"Prepared By "}
-        <Text
-          style={styles.link}
-          onPress={() => Linking.openURL("https://reactnativeschool.com")}
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => onOnboardingComplete()}
+          activeOpacity={0.8}
         >
-          React Native School
-        </Text>
+          <Text style={styles.btnText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+      <Image style={styles.logo} source={require("../assets/RNS.png")} />
+      <Text
+        style={styles.text}
+        onPress={() => Linking.openURL("https://reactnativeschool.com")}
+      >
+        Prepared by React Native School
       </Text>
     </SafeAreaView>
   );
