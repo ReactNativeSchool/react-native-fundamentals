f71069375e1bae611cd792db9c298e3b09b9b2c7
Spencer Carli <spencercarli@gmail.com>
2020-09-02 12:03:49 -0500

    Basic md styling

--- src/screens/Instructions.js
+++ src/screens/Instructions.js
@@ -1,10 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import _ from "lodash";
 import { Asset } from "expo-asset";
 import Markdown from "react-native-markdown-display";
 
 import exercises from "../../Exercises";
+import colors from "../constants/colors";
+
+const mdStyles = {
+  heading1: {
+    flexDirection: "row",
+    fontSize: 32,
+    borderBottomColor: "rgba(27,31,35,.2)",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginBottom: 10,
+    paddingBottom: 5,
+    marginTop: 10,
+  },
+  heading2: {
+    flexDirection: "row",
+    fontSize: 24,
+    borderBottomColor: "rgba(27,31,35,.2)",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    paddingBottom: 5,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  fence: {
+    marginVertical: 10,
+  },
+  link: {
+    color: colors.link,
+  },
+};
 
 export const Instructions = ({ route }) => {
   const lesson = _.get(route, "params.lesson");
@@ -34,7 +62,9 @@ export const Instructions = ({ route }) => {
         backgroundColor: "#fff",
       }}
     >
-      <Markdown>{content}</Markdown>
+      <Markdown style={mdStyles} mergeStyle>
+        {content}
+      </Markdown>
       <View style={{ width: 1, height: 200, backgroundColor: "transparent" }} />
     </ScrollView>
   );
