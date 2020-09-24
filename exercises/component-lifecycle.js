81e23b3fb6f402d19626c977dde7ec91b6b26dff
Spencer Carli <spencercarli@gmail.com>
2020-09-24 10:49:45 -0500

    Add listener2 import

--- exercises/component-lifecycle.js
+++ exercises/component-lifecycle.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
 
-import { listener } from "../support/component-lifecycle";
+import { listener, listener2 } from "../support/component-lifecycle";
 
 export default () => {
   const [updateCount, setUpdateCount] = useState(0);
