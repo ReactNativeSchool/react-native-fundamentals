333922ce855ba0775973b5e6aeb202857cde3373
Hui <xuhuizhu369@gmail.com>
2021-06-09 07:41:49 -0700

    captures tap with touchableOpacity and alerts hello

--- exercises/capturing-taps.js
+++ exercises/capturing-taps.js
@@ -1,9 +1,39 @@
 // Capturing User Taps
-
+import React from 'react';
+import { Button, Text, TouchableOpacity, Alert, StyleSheet, Dimensions } from "react-native";
 // TIP: Button is a component exposed by React Native. You'll import it the same way you did View
 // and Text previously.
 
 export default () => {
   // Replace return null with your code.
-  return null;
+
+  const alertHello = () => {
+    Alert.alert('Hello Alert', 'Hellow!')
+  }
+
+  // return (
+  //   <Button
+  //     title='My Button'
+  //     onPress={alertHello}
+  //   />
+  // )
+
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={alertHello}
+    >
+      <Text>Click Me</Text>
+    </TouchableOpacity>
+  )
 };
+
+const styles = StyleSheet.create({
+  button: {
+    width: Dimensions.get('window').width,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink'
+  }
+});
