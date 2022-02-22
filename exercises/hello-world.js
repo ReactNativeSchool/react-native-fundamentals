6314eecaf4b3dc11721768cf48631bc01953032c
jason jxsn <99940146+jasonjxsn@users.noreply.github.com>
2022-02-21 21:45:18 -0900

    First Exercise done

--- exercises/hello-world.js
+++ exercises/hello-world.js
@@ -1,14 +1,28 @@
 // Hello, World!
 
 // When using JSX React needs to be in scope. This means you have to import it.
-// import React from "react";
+import React from "react";
 
 // Unlike working with HTML, the building blocks of our UI are not global. Therefore we need to
 // import them.
-// import { View, Text } from "react-native";
+import { View, Text } from "react-native";
 
 // TIP: A component is simply a function from which we return JSX.
-// const App = () => {}
+const App = () => {
+    return (
+        <View
+            style={{
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "lightblue", 
+                height: 100,
+                width: 100,
+            }}
+        >
+            <Text>Hello World!</Text>
+        </View>
+    );
+};
 
 // TIP: Components can nest and have children. <View><Text></Text></View>
 
@@ -17,4 +31,4 @@
 
 // We need to export App/Component so it's rendered on the screen. To work in this example app you
 // need to use export default. Dont' worry about this right now, we'll talk about it later.
-// export default App;
+export default App;
