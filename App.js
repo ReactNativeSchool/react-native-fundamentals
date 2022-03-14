d49aebd3266810c2997d3da2664f0267015277ec
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-03-14 13:15:32 -0500

    Upgrade deps

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+import { LogBox } from "react-native";
 import { App } from "./src/index";
+
+LogBox.ignoreLogs([
+  // Courier isn't used anywhere in the app. I'm not sure why it's throwing this warning.
+  'fontFamily "Courier" is not a system font and has not been loaded through Font.loadAsync.',
+]);
 
 export default App;
